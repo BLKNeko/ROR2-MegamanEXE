f3d55cc897a55cd68653726c60740e89d408c860
BLKNeko <blackheart2302@gmail.com>
2023-05-09 15:46:08 -0300

    upload

---
+++ MegamanEXEMod/Modules/States.cs
@@ -0,0 +1,22 @@
+﻿using MegamanEXEMod.SkillStates;
+using MegamanEXEMod.SkillStates.BaseStates;
+using System.Collections.Generic;
+using System;
+
+namespace MegamanEXEMod.Modules
+{
+    public static class States
+    {
+        internal static void RegisterStates()
+        {
+            Modules.Content.AddEntityState(typeof(BaseMeleeAttack));
+            Modules.Content.AddEntityState(typeof(SlashCombo));
+
+            Modules.Content.AddEntityState(typeof(Shoot));
+
+            Modules.Content.AddEntityState(typeof(Roll));
+
+            Modules.Content.AddEntityState(typeof(ThrowBomb));
+        }
+    }
+}
